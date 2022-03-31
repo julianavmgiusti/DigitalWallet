5392fd34ef9ac0f9fe1cb73d4c46eeddb7554feb
Juliana Giusti <juliana.vmelo@gmail.com>
2022-03-31 15:18:04 -0300

    Iniciando o projeto e  Login Page e Wallet page

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+export const USER_EMAIL = 'USER_EMAIL';
+
+export const LOGIN = (email) => ({
+  type: USER_EMAIL,
+  payload: email,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,82 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { LOGIN } from '../actions';
 
 class Login extends React.Component {
+    state = {
+      email: '',
+      password: '',
+      disabled: true,
+    };
+
+  handleInputChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value }, this.bttnLogInValidation);
+  };
+
+  isAValidEmail = (email) => {
+    // ref: https://stackoverflow.com/questions/46155/whats-the-best-way-to-valnameate-an-email-address-in-javascript
+    const emailVerification = /\S+@\S+\.\S+/;
+    return emailVerification.test(email);
+  }
+
+  bttnLogInValidation = () => {
+    const minLengthPassword = 6;
+    const { email, password } = this.state;
+    const isValidPassword = password.length >= minLengthPassword;
+    if ((this.isAValidEmail(email)) && isValidPassword) {
+      this.setState({ disabled: false });
+    } else {
+      this.setState({ disabled: true });
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, disabled } = this.state;
+    const { logged, history } = this.props;
+
+    return (
+      <form>
+        <h1>Login</h1>
+        <input
+          type="email"
+          value={ email }
+          name="email"
+          onChange={ this.handleInputChange }
+          placeholder="e-mail"
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          value={ password }
+          name="password"
+          onChange={ this.handleInputChange }
+          placeholder="senha"
+          data-testid="password-input"
+        />
+        <button
+          type="button"
+          disabled={ disabled }
+          onClick={ () => {
+            logged(email);
+            history.push('/carteira');
+          } }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+  logged: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  logged: (email) => dispatch(LOGIN(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
