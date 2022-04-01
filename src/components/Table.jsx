43c7318650a5cb543c918b8ca8173b4b89ce4eed
Juliana Giusti <juliana.vmelo@gmail.com>
2022-04-01 00:35:07 -0300

    Table with global state

---
+++ src/components/Table.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Table extends Component {
+  render() {
+    const {
+      wallet: {
+        expenses,
+      },
+    } = this.props;
+
+    return (
+      <>
+        <h2>Tabela de despesas</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            { expenses
+              .map(({ id, description, tag, method, value, exchangeRates, currency }) => {
+                const exchange = Number(exchangeRates[currency].ask);
+                const currencyName = exchangeRates[currency].name;
+                const currentValueOfCurrency = Number(value).toFixed(2);
+                const convertedValue = (exchange * Number(value)).toFixed(2);
+
+                return (
+                  <tr key={ id }>
+                    <td>{description}</td>
+                    <td>{tag}</td>
+                    <td>{method}</td>
+                    <td>{currentValueOfCurrency}</td>
+                    <td>{currencyName}</td>
+                    <td>{exchange.toFixed(2)}</td>
+                    <td>{convertedValue}</td>
+                    <td>Real</td>
+                    <td>
+                      <button
+                        data-testid="edit-btn"
+                        type="button"
+                      >
+                        Editar
+                      </button>
+                      <button
+                        data-testid="delete-btn"
+                        type="button"
+                      >
+                        Excluir
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
+
+Table.propTypes = {
+  dispatch: PropTypes.func,
+  wallet: PropTypes.shape({
+    expenses: PropTypes.arrayOf(PropTypes.obj),
+  }),
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  wallet: state.wallet,
+});
+
+export default connect(mapStateToProps)(Table);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ExpensesForm from '../components/ExpensesForm';
 import Header from '../components/Header';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <ExpensesForm />
+        <Table />
       </div>
     );
   }
